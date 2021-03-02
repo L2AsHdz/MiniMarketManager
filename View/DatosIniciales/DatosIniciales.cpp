eedf89d603d9d56e2a9da3d2c1e0d0fc44f328b9
L2AsHdz <leonidas0197@gmail.com>
2021-03-02 11:03:17 -0600

    Metodos para solicitar datos creados

---
+++ View/DatosIniciales/DatosIniciales.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+
+using namespace std;
+
+void bienvenida() {
+    cout<<"---------------MiniMarket---------------"<<endl;
+    cout<<"A continuacion se le solicitaran los datos necesarios para la simulacion"<<endl;
+}
+
+int solicitarClientesEnEspera() {
+    int cantClientes;
+    
+    cout<<endl<<"Ingrese la cantidad de clientes en la cola de espera ";
+    cin>>cantClientes;
+
+    return cantClientes;
+}
+
+int solicitarNumeroCarretas(int p) {
+    int cantCarretas;
+
+    cout<<endl<<"Ingrese la cantidad de carretas en la pila "<<p<<" ";
+    cin>>cantCarretas;
+
+    return cantCarretas;
+}
+
+int solicitarClientesComprando() {
+    int cantClientes;
+
+    cout<<endl<<"Ingrese la cantidad de clientes comprando ";
+    cin>>cantClientes;
+
+    return cantClientes;
+}
+
+int solicitarClientesColaPago() {
+    int cantClientes;
+
+    cout<<endl<<"Ingrese la cantidad de clientes en la cola de pago ";
+    cin>>cantClientes;
+
+    return cantClientes; 
+}
+
+int solicitarCantidadCajas() {
+    int cantCajas;
+
+    cout<<endl<<"Ingrese la cantidad de cajas ";
+    cin>> cantCajas;
+
+    return cantCajas;
+}
---
+++ View/DatosIniciales/Vista.h
@@ -0,0 +1,11 @@
+void bienvenida();
+
+int solicitarClientesEnEspera();
+
+int solicitarNumeroCarretas(int);
+
+int solicitarClientesComprando();
+
+int solicitarClientesColaPago();
+
+int solicitarCantidadCajas();
