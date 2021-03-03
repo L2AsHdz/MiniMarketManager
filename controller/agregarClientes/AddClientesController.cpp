e0d209dc3192c34c3b8ca7abebdd56eacda05b08
L2AsHdz <leonidas0197@gmail.com>
2021-03-02 19:52:43 -0600

    Logica para agregar clientes agregada

--- controller/agregarClientes/AddClientesController.cpp
+++ controller/agregarClientes/AddClientesController.cpp
@@ -6,8 +6,23 @@ using namespace std;
 AddClientesController::AddClientesController(){}
 
 void AddClientesController::start(Pila* carretas1, Pila* carretas2, 
-Cola* clientesEspera, int idCliente, int cantClientes) {
-    /*if () {
-
-    }*/
+Cola* clientesEspera, CircularList* clientesCompra, int idCliente, int cantClientes) {
+    Cliente cliente;
+    int idClienteInicio = idCliente;
+    int carreta;
+    
+    for (idCliente; idCliente < (idClienteInicio + cantClientes); idCliente++) {
+        cout<<"El cliente "<<idCliente<<" llega a la tienda."<<endl;
+        if (!carretas1->isEmpty() & clientesEspera->isEmpty()) {
+            carreta = carretas1->pop();
+            cout<<"La carreta "<<carreta<< " es tomada por el cliente "<<idCliente<<"."<<endl;
+            clientesCompra->agregar(Cliente(idCliente, carreta));
+        } else if (carretas2->isEmpty() & clientesEspera->isEmpty()) {
+            carreta = carretas2->pop();
+            cout<<"La carreta "<<carreta<< " es tomada por el cliente "<<idCliente<<"."<<endl;
+            clientesCompra->agregar(Cliente(idCliente, carretas2->pop()));
+        } else {
+            clientesEspera->insertar(Cliente(idCliente));
+        }
+    }
 }
--- controller/agregarClientes/AddClientesController.h
+++ controller/agregarClientes/AddClientesController.h
@@ -3,11 +3,12 @@
 
 #include "../../edd/pila/Pila.h"
 #include "../../edd/cola/Cola.h"
+#include "../../edd/circularList/CircularList.h"
 
 class AddClientesController {
     public:
         AddClientesController();
-        void start(Pila*, Pila*, Cola*, int, int);
+        void start(Pila*, Pila*, Cola*, CircularList*, int, int);
 };
 
 #endif /*ADD_CLIENTES_CONTROLLER_H*/
