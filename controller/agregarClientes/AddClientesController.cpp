8aed00e009056c37214e818611a98cd2bcfe4c15
L2AsHdz <leonidas0197@gmail.com>
2021-03-02 20:01:09 -0600

    Optimizacion de logica para agregar nuevos clientes

--- controller/agregarClientes/AddClientesController.cpp
+++ controller/agregarClientes/AddClientesController.cpp
@@ -7,22 +7,21 @@ AddClientesController::AddClientesController(){}
 
 void AddClientesController::start(Pila* carretas1, Pila* carretas2, 
 Cola* clientesEspera, CircularList* clientesCompra, int idCliente, int cantClientes) {
-    Cliente cliente;
     int idClienteInicio = idCliente;
-    int carreta;
     
     for (idCliente; idCliente < (idClienteInicio + cantClientes); idCliente++) {
         cout<<"El cliente "<<idCliente<<" llega a la tienda."<<endl;
         if (!carretas1->isEmpty() & clientesEspera->isEmpty()) {
-            carreta = carretas1->pop();
-            cout<<"La carreta "<<carreta<< " es tomada por el cliente "<<idCliente<<"."<<endl;
-            clientesCompra->agregar(Cliente(idCliente, carreta));
-        } else if (carretas2->isEmpty() & clientesEspera->isEmpty()) {
-            carreta = carretas2->pop();
-            cout<<"La carreta "<<carreta<< " es tomada por el cliente "<<idCliente<<"."<<endl;
-            clientesCompra->agregar(Cliente(idCliente, carretas2->pop()));
+            agregarClienteACompras(clientesCompra, idCliente, carretas1->pop());
+        } else if (!carretas2->isEmpty() & clientesEspera->isEmpty()) {
+            agregarClienteACompras(clientesCompra, idCliente, carretas2->pop());
         } else {
             clientesEspera->insertar(Cliente(idCliente));
         }
     }
+}
+
+void agregarClienteACompras(CircularList* clientesCompra, int id, int carreta) {
+    cout<<"La carreta "<<carreta<< " es tomada por el cliente "<<id<<"."<<endl;
+    clientesCompra->agregar(Cliente(id, carreta));
 }
